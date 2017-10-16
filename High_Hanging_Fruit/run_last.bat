1cb3b7301a72c62c6fcd8e7cb3e8ff3e8e873ab9
Shaun Wu <alexbay218@gmail.com>
2017-10-16 12:14:35 -0400

    Update to prevent account lockout

--- High_Hanging_Fruit/run_last.bat
+++ High_Hanging_Fruit/run_last.bat
@@ -10,6 +10,8 @@ Start "Advanced Firewall" /wait "%~dp0\advfirewall\VCaftApplier"
 
 secedit /configure /db "%~dp0secedit/secedit.sdb" /cfg "%~dp0secedit/VClsp.inf"
 
+net user %USERNAME% /active:yes
+
 Start "End" "%~dp0..\AutoHotKey_Source\AutoHotkey.exe" "%~dp0\end.ahk"
 
 echo exit > "%~dp0\_cliplog\exit.txt"
--- Injector/compile.bat
+++ Injector/compile.bat
@@ -1,2 +1,2 @@
-g++.exe -o "%cd%\bin\main.exe" -O4 "%cd%\main.cpp"
+g++.exe -o "%cd%\bin\main.exe" -O4 -static "%cd%\main.cpp"
 Pause
