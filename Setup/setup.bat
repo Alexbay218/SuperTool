e40473e805d4c5a97a804f7d55e787a435980e31
Alexbay218 <Alexbay218@gmail.com>
2017-11-02 14:48:37 -0400

    ninite auto install

---
+++ Setup/setup.bat
@@ -0,0 +1,4 @@
+cd %~dp0
+"%~dp0wget\bin\wget.exe" --no-check-certificate "http://ninite.com/.net4.7-7zip-chrome-essentials-firefox-java8-malwarebytes-notepadplusplus-spybot2/ninite.exe"
+"%~dp0ninite.exe"
+pause
