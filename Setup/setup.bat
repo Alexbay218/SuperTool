794d9b2d6f9bca921a30bc8b9b26a26e1fa552fb
Shaun Wu <Alexbay218@gmail.com>
2017-11-03 09:18:59 -0400

    Update setup.bat to prevent premature exit

--- Setup/setup.bat
+++ Setup/setup.bat
@@ -2,8 +2,10 @@ cd %~dp0
 rmdir /s /q "%~dp0..\Archive\Data\Before"
 mkdir "%~dp0..\Archive\Data\Before"
 "%~dp0..\Archive\DISMLister\list.bat"
-move "%~dp0..\Archive\DISMLister\feature_list.txt" "%~dp0..\Archive\Data\Before"
-move "%~dp0..\Archive\DISMLister\package_list.txt" "%~dp0..\Archive\Data\Before"
+dism /online /get-features > feature_list.txt
+dism /online /get-package > package_list.txt
+move "%~dp0feature_list.txt" "%~dp0..\Archive\Data\Before"
+move "%~dp0package_list.txt" "%~dp0..\Archive\Data\Before"
 "%~dp0..\Archive\HashShot\run.bat"
 move "%~dp0..\Archive\HashShot\bin\log.txt" "%~dp0..\Archive\Data\Before"
 cd %~dp0
@@ -12,9 +14,10 @@ cd %~dp0
 cd %~dp0
 rmdir /s /q "%~dp0..\Archive\Data\Before"
 mkdir "%~dp0..\Archive\Data\After"
-"%~dp0..\Archive\DISMLister\list.bat"
-move "%~dp0..\Archive\DISMLister\feature_list.txt" "%~dp0..\Archive\Data\After"
-move "%~dp0..\Archive\DISMLister\package_list.txt" "%~dp0..\Archive\Data\After"
+dism /online /get-features > feature_list.txt
+dism /online /get-package > package_list.txt
+move "%~dp0feature_list.txt" "%~dp0..\Archive\Data\After"
+move "%~dp0package_list.txt" "%~dp0..\Archive\Data\After"
 "%~dp0..\Archive\HashShot\run.bat"
 move "%~dp0..\Archive\HashShot\bin\log.txt" "%~dp0..\Archive\Data\After"
 cd %~dp0
