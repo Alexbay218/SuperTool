9a54f3f06e72a0aecee386ecf51c4eabfa972830
Alexbay218 <Alexbay218@gmail.com>
2018-02-06 23:34:03 -0500

    Quick procdump change

--- Archive/dump.bat
+++ Archive/dump.bat
@@ -5,6 +5,6 @@ setlocal EnableExtensions
 for /f "tokens=2* delims==" %%G IN ('
         wmic process get name /value  2^>NUL
     ') DO for %%g in (%%~G) do (
-	"%~dp0..\Sysinternals_Source\procdump.exe" -ma %%~g C:\ >> "%~dp0%version%\dump\%%~g.dmp"
+	"%~dp0..\Sysinternals_Source\procdump.exe" -ma %%~g "%~dp0%version%\dump\%%~g.dmp"
     )
 "%~dp0logons.bat"
