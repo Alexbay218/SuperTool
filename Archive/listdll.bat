7d5f29b9789158e388601c75c2c8532d89d0e31f
Alexbay218 <Alexbay218@gmail.com>
2018-02-07 12:55:31 -0500

    Remove dumping capabilities for speed

--- Archive/listdll.bat
+++ Archive/listdll.bat
@@ -2,4 +2,4 @@ for /f "tokens=*" %%a in ('ver') do set version=%%a
 mkdir "%~dp0%version%"
 mkdir "%~dp0%version%\listdll"
 "%~dp0..\Sysinternals_Source\listdll.exe" -u > "%~dp0%version%\listdll\dlls.txt"
-"%~dp0dump.bat"
+"%~dp0logons.bat"
