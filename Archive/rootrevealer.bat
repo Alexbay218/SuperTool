9d60840609f5f122d1ea90f68fb3e7b7bdd54061
Alexbay218 <Alexbay218@gmail.com>
2018-02-07 13:04:01 -0500

    Remove rootkitscanner for speed purposes

--- Archive/rootrevealer.bat
+++
@@ -1,4 +0,0 @@
-for /f "tokens=*" %%a in ('ver') do set version=%%a
-mkdir "%~dp0%version%"
-mkdir "%~dp0%version%\rootrevealer"
-"%~dp0..\Sysinternals_Source\rootkitrevealer.exe" -a -m -c "%~dp0%version%\rootrevealer\rkr.csv"
