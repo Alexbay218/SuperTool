a5c6c1a2768e63656ba9f4ce098c15879550cfbe
Alexbay218 <Alexbay218@gmail.com>
2018-02-09 14:36:17 -0500

    Update setup and firewall

--- High_Hanging_Fruit/run_last.bat
+++ High_Hanging_Fruit/run_last.bat
@@ -2,7 +2,7 @@ Start "Start" "%~dp0..\AutoHotKey_Source\AutoHotkey.exe" "%~dp0start.ahk"
 
 Timeout 2
 
-Rem Start "Advanced Firewall" /wait "%~dp0\advfirewall\aftApplier.bat"
+start "Advanced Firewall" /wait "%~dp0advfirewall\aftApplier.bat"
 
 echo exit > "%~dp0exit.txt"
 
--- Setup/setup.bat
+++ Setup/setup.bat
@@ -1,6 +1,10 @@
-Start "Start" "%~dp0..\AutoHotKey_Source\AutoHotkey.exe" "%~dp0start.ahk"
+start "Start" "%~dp0..\AutoHotKey_Source\AutoHotkey.exe" "%~dp0start.ahk"
 
 start "Start Collect" /W "%~dp0..\Archive\start_collect.bat"
+
+"%~dp0wget\bin\wget.exe" --no-check-certificate "http://ninite.com/.net4.7-7zip-chrome-essentials-firefox-java8-malwarebytes-notepadplusplus-spybot2/ninite.exe"
+
+timeout 2
 
 "%~dp0wget\bin\wget.exe" --no-check-certificate "http://ninite.com/.net4.7-7zip-chrome-essentials-firefox-java8-malwarebytes-notepadplusplus-spybot2/ninite.exe"
 
